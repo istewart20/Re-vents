d15fcbfda6ae72c0505c695dd46dff9ac0831133
Iain <iain.stewart08@gmail.com>
2021-03-08 00:30:45 +0000

    Add semantic UI + sample button

--- src/App/Layout/App.jsx
+++ src/App/Layout/App.jsx
@@ -1,9 +1,11 @@
+import { Button } from 'semantic-ui-react';
 import './styles.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Re-vents</h1>
+      <Button icon='user' content='React button' color='green'></Button>
     </div>
   );
 }
